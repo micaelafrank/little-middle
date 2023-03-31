47922ad79b0c476fc60915a7d297d8e2764d1afa
Micaela Frank <micaelalfrank@gmail.com>
2023-03-30 17:31:15 -0700

    image gallery up and running

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -7,11 +7,44 @@ function ImageGallery(){
         <>
             <NavBar/>
             <h1 className="pageTitle">Image Gallery</h1>
-            <img alt="Dough on its first rise" src="/images/challah0.jpeg" />
             <div className="flexRowGrid">
-                <div className="imageContainer">
-                    <img className="imageStyle" alt="Dough on its first rise" src="/images/challah0.jpeg" />
+                <div className="oneImageContainer">
+                    <div id="pic1" className="imageStyle"/>
+                    <p className="imageCaption">22 lb bag of flour</p>
                 </div>
+                <div className="oneImageContainer">
+                    <div id="pic2" className="imageStyle" />
+                    <p className="imageCaption">Rising dough!</p>
+                </div>
+                <div className="oneImageContainer">
+                    <div id="pic3" className="imageStyle" />
+                    <p className="imageCaption">Orders waiting to be packaged</p>
+                </div>
+                <div className="oneImageContainer">
+                    <div id="pic7" className="imageStyle" />
+                    <p className="imageCaption">Freshly baked challahs</p>
+                </div>
+                <div className="oneImageContainer">
+                    <div id="pic8" className="imageStyle" />
+                    <p className="imageCaption">Braided dough in the middle of their last rise</p>
+                </div>
+                <div className="oneImageContainer">
+                    <div id="pic10" className="imageStyle" />
+                    <p className="imageCaption">Special rainbow edition</p>
+                </div>
+                <div className="oneImageContainer">
+                    <div id="pic4" className="imageStyle" />
+                    <p className="imageCaption">Packaged orders</p>
+                </div>
+                <div className="oneImageContainer">
+                    <div id="pic6" className="imageStyle" />
+                    <p className="imageCaption">More orders</p>
+                </div>
+                <div className="oneImageContainer">
+                    <div id="pic11" className="imageStyle" />
+                    <p className="imageCaption">Ready to be picked up!</p>
+                </div>
+
                 {/* <div className="imageContainer">
                     <img id="pic2" className="imageStyle" alt="Image 2" />
                 </div>
